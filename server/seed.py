a042280c6d3d0bde58c082a4984070d370fa85e5
SamuelWade <samuelrcwade@gmail.com>
2024-02-12 12:22:42 -0600

    last commit before ssd card upgrade 2

--- server/seed.py
+++ server/seed.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 from faker import Faker
 import random
 
-from config import app  # Make sure this imports your Flask app correctly
+from config import app  
 
 faker = Faker()
 
@@ -19,31 +19,37 @@ def clear_data():
 def create_user():
     user = User(username=faker.user_name(), email=faker.email(), password_hash=faker.sha256())
     db.session.add(user)
+    db.session.commit()
     return user
 
 def create_company(user):
     company = Company(name=faker.company(), manager=user)
     db.session.add(company)
+    db.session.commit()
     return company
 
 def create_contact(company):
     contact = Contact(name=faker.name(), last_contact_date=datetime.now(), status=random.choice(["hot", "warm", "cold"]), company=company, manager=company.manager)
     db.session.add(contact)
+    db.session.commit()
     return contact
 
 def create_todo_list(contact):
     todo_list = ToDoList(title=faker.bs(), contact_id=contact.id)
     db.session.add(todo_list)
+    db.session.commit()
     return todo_list
 
 def create_todo_item(todo_list):
     todo = ToDo(title=faker.catch_phrase(), description=faker.sentence(), completed=random.choice([True, False]), date_created=datetime.now(), due_date=datetime.now() + timedelta(days=random.randint(1, 10)), list_id=todo_list.id)
     db.session.add(todo)
+    db.session.commit()
     return todo
 
 def create_tag():
     tag = Tag(name=faker.word())
     db.session.add(tag)
+    db.session.commit()
     return tag
 
 def assign_tag_to_todo(todo, tag):
