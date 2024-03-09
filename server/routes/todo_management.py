91d7e4be397da12c69e27b2224f6a5fb50afb4af
swade-55 <samuelrcwade@gmail.com>
2024-03-09 12:18:20 -0600

    modified todo flask route to save tags

--- server/routes/todo_management.py
+++ server/routes/todo_management.py
@@ -1,5 +1,5 @@
 from flask import jsonify, request
-from models import db, ToDo
+from models import db, ToDo, Tag
 from config import app
 from datetime import datetime
 
@@ -10,16 +10,29 @@ def add_todo():
     due_date = data.get('dueDate')
     if due_date:
         due_date = datetime.fromisoformat(due_date.rstrip('Z'))
+    
+    # Create the new ToDo instance
     new_todo = ToDo(
         title=data['title'],
         description=data['description'],
-        completed = data['completed'],
-        due_date = due_date,
-        list_id=data['listId'], 
-        
+        completed=data['completed'],
+        due_date=due_date,
+        list_id=data['listId'],
     )
+    
+    # Process tags if provided
+    tag_ids = data.get('tags', []) 
+    for tag_id in tag_ids:
+        tag = Tag.query.get(tag_id) 
+        if tag:
+            new_todo.tags.append(tag)  
+        else:
+            pass
+    
     db.session.add(new_todo)
     db.session.commit()
+    
+    # Modify the return to include tags in the response if needed
     return jsonify(new_todo.to_dict()), 201
 
 @app.route('/todos/<int:list_id>', methods=['GET'])
